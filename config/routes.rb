83565813a2b63593f21f28c649ae3a4bf7f00702
Eugeniy Kalinin <burump@gmail.com>
2012-05-02 23:08:44 -0700

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,10 @@ Bus::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'application#index'
+
+  # Catch all route for the Backbone
+  match '*path', to: 'application#index'
 
   # See how all your routes lay out with "rake routes"
 
