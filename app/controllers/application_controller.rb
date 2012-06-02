7591d3c5ed043b55fb2c526b9c71a1556460555e
Gilad Gray <giladgray@gmail.com>
2012-06-02 14:42:11 -0700

    first commit. rails+spine.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+	protect_from_forgery
+
+	before_filter :cor
+
+	def cor
+		headers["Access-Control-Allow-Origin"]  = "js-app-origin.com"
+		headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+		headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+		head(:ok) if request.request_method == "OPTIONS"
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhichbusSpine::Application.config.secret_token = 'daf103bd11e8580ae0c4890214e36b0c7ca4ff2dae820a37dc7a426d3a39573fb4659a9339ae61b5a57af0f701370f33569ca8d60a447baec812b00619e6f0bd'
