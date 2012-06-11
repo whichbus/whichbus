61e76150fa9b6ca6fdabccb2b4f5bcd9c82a0b0c
Gilad Gray <giladgray@gmail.com>
2012-06-11 02:31:48 -0700

    snazz up the UI with bootstrap buttons!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
 	def agency_item(agency)
-		"<li class='agency'><h3>#{link_to agency.name, agency}</h3></li>"
+		"<li class='agency'><h3>#{link_to agency.name, agency, class: 'btn btn-large btn-info'}</h3></li>"
 	end
 
 	def route_item(route)
-		"<li class='route'><h3>#{link_to route.name, route}</h3></li>"
+		"<li class='route'><h3>#{link_to route.name, route, class: 'btn btn-large btn-success'}</h3></li>"
 	end
 
 	def stop_item(stop)
-		"<li class='stop'><h3>#{link_to stop.name, stop}</h3></li>"
+		"<li class='stop'><h3>#{link_to stop.name, stop, class: 'btn btn-large btn-warning'}</h3></li>"
 	end
 end
