133ed0400bfdc4b1538786b13b88ffc0ff5e5279
Eugeniy Kalinin <burump@gmail.com>
2012-06-27 18:36:25 -0700

    removed rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Bus::Application.config.secret_token = '42d27960cf4ec0ce5d3eebe17db658f35285ba6c56a8f5a27a3a28b8e4ddf8b090092f0236c1e2661c722c94671628380bd13464a3bdebaa08ec794c1e9a026c'
