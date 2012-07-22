8d254a84e1aa7714258428c1c42c653f700adad0
schulman <joeschulman@gmail.com>
2012-07-22 15:43:35 -0700

    Fixes closes #31

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ WhichbusSpine::Application.routes.draw do
   get "pages/index"
 
   match '/spine' => 'pages#index'
-  match '/about/:action' => 'about#:action'
+  # If we choose to add more pages without updating routes, use this approach
+  #match '/about/:action' => 'about#:action'
+  match '/about' => 'about#whichbus'
+  match '/privacy' => 'about#privacy'
+  match '/terms' => 'about#terms'
+  match '/issues' => 'about#issues'
 
   scope '/workshop' do
     match '' => 'application#search', :as => :workshop
